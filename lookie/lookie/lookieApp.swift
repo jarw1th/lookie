8fe9036bb1f5d48f84f58d2ba7531c5334c74cbe
parastaev <parastaev.r@softintermob.com>
2024-08-07 16:42:08 +0300

    Initial

---
+++ lookie/lookie/lookieApp.swift
@@ -0,0 +1,17 @@
+//
+//  lookieApp.swift
+//  lookie
+//
+//  Created by Руслан Парастаев on 07.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct lookieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
