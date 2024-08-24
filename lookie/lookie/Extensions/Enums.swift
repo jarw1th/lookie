c46b74068083681569883a143388a5b92c0b5f23
jarw1th <parastaev.r@softintermob.com>
2024-08-24 23:06:54 +0300

    feat: Added feed type bar for Feed screen

--- lookie/lookie/Extensions/Enums.swift
+++ lookie/lookie/Extensions/Enums.swift
@@ -6,3 +6,35 @@ enum TabType {
     case home, search, profile
     
 }
+
+enum FeedType: CaseIterable {
+    
+    case none, stylists, outfits, shoes
+    
+    func text() -> String? {
+        switch self {
+        case .none:
+            return nil
+        case .stylists:
+            return "Stylists"
+        case .outfits:
+            return "Outfits"
+        case .shoes:
+            return "Shoes"
+        }
+    }
+    
+    func isPremium() -> Bool {
+        switch self {
+        case .none:
+            return false
+        case .stylists:
+            return true
+        case .outfits:
+            return false
+        case .shoes:
+            return false
+        }
+    }
+    
+}
